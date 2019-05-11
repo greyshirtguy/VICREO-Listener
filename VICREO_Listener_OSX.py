ed6765aa69fd9a984396ee6546d38c2083c573ee
Jeffrey Davidsz <jeffrey.davidsz@vicreo.eu>
2019-05-11 09:42:56 +0200

    Rewrite of OSX listener using Rumps and threads

--- VICREO_Listener_OSX.py
+++ VICREO_Listener_OSX.py
@@ -3,203 +3,206 @@ import socket
 import sys
 # file handling
 import os
-import subprocess
+#RUMPS for menu
+import rumps
+import time
+
 #keyboard
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
-def pressAndRelease(key):
-	keyboard.press(key)
-	keyboard.release(key)
+rumps.debug_mode(False) # turn on command line logging information for development - default is off
 
-#https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
-modifier = {
-	'alt': Key.alt,
-	'ctrl': Key.ctrl,
-	'tab': Key.tab,
-	'shift': Key.shift,
-	'cmd': Key.cmd,
-	'alt_gr': Key.alt_gr,
-	'delete': Key.delete,
-	'space': Key.space,
-	'backspace': Key.backspace,
-	'caps_lock': Key.caps_lock,
-	'end': Key.end,
-	'enter': Key.enter,
-	'esc': Key.esc,
-	'f1': Key.f1,
-	'f2': Key.f2,
-	'f3': Key.f3,
-	'f4': Key.f4,
-	'f5': Key.f5,
-	'f6': Key.f6,
-	'f7': Key.f7,
-	'f8': Key.f8,
-	'f9': Key.f9,
-	'f10': Key.f10,
-	'f11': Key.f11,
-	'f12': Key.f12,
-	'home': Key.home,
-	'left': Key.left,
-	'right': Key.right,
-	'up': Key.up,
-	'down': Key.down,
-	'page_up': Key.page_up,
-	'page_down': Key.page_down
-	}
+from threading import Thread
 
-#Start main function
-_LOOP = True
+def myRumps():
+	@rumps.clicked("About")
+	def about(sender):
+		rumps.alert("Welcome at the VICREO Listener")
 
-def openFile(path):
-	path = path.replace('\\', '/')
-	path = path.strip()
-	if sys.platform.find('darwin') >= 0:
-		print('darwin', path)
-		subprocess.call(['open', path])
-	elif sys.platform.find('linux') >= 0:
-		print('linux')
-		subprocess.call(['xdg-open', path])
-	else:
-		print('something else')
+	@rumps.notifications
+	def notifications(notification):  # function that reacts to incoming notification dicts
+		print(notification)
+
+
+	def onebitcallback(sender):  # functions don't have to be decorated to serve as callbacks for buttons
+		print(4848484) # this function is specified as a callback when creating a MenuItem below
+
+	def pressAndRelease(key):
+		keyboard.press(key)
+		keyboard.release(key)
+
+	app = rumps.App("VICREO Listener", title='VICREO')
+	app.menu = [
+		rumps.MenuItem('About', icon='icon.ico', dimensions=(18, 18)),  # can specify an icon to be placed near text
+		None
+	]
+	app.run()
+
+def myListener():
+	#https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
+	modifier = {
+		'alt': Key.alt,
+		'ctrl': Key.ctrl,
+		'tab': Key.tab,
+		'shift': Key.shift,
+		'cmd': Key.cmd,
+		'alt_gr': Key.alt_gr,
+		'delete': Key.delete,
+		'space': Key.space,
+		'backspace': Key.backspace,
+		'caps_lock': Key.caps_lock,
+		'end': Key.end,
+		'enter': Key.enter,
+		'esc': Key.esc,
+		'f1': Key.f1,
+		'f2': Key.f2,
+		'f3': Key.f3,
+		'f4': Key.f4,
+		'f5': Key.f5,
+		'f6': Key.f6,
+		'f7': Key.f7,
+		'f8': Key.f8,
+		'f9': Key.f9,
+		'f10': Key.f10,
+		'f11': Key.f11,
+		'f12': Key.f12,
+		'home': Key.home,
+		'left': Key.left,
+		'right': Key.right,
+		'up': Key.up,
+		'down': Key.down,
+		'page_up': Key.page_up,
+		'page_down': Key.page_down
+		}
+
+
+	_LOOP = True
+
+	def openFile(path):
 		os.startfile(path)
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the port
-server_address = ('', 10001)
-print('Listening to port 10001')
-sock.bind(server_address)
-# Listen for incoming connections
-sock.listen(1)
+	# Create a TCP/IP socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# Bind the socket to the port
+	server_address = ('', 10001)
+	print('Listening to port 10001')
+	sock.bind(server_address)
+	# Listen for incoming connections
+	sock.listen(1)
 
-while _LOOP:
-	# Wait for a connection
-	print('waiting for a connection')
-	connection, client_address = sock.accept()
+	#Start main function
+	while _LOOP:
+		# Wait for a connection
+		print('waiting for a connection')
+		connection, client_address = sock.accept()
+		try:
+			# Receive the data and retransmit it
+			print('connection from', client_address)
+			while _LOOP:
+				data = connection.recv(160)
+				if data:
+					tcpString = data.decode()
+					print('Receiving: ', tcpString)
+					# Single key command
+					if tcpString[0:4] == '<SK>':
+						#somehow on mac capital letters returns: a
+						sendedKey = tcpString[4]
+						if sendedKey.isupper():
+							keyboard.press(Key.shift)
+							keyboard.press(sendedKey.lower())
+							keyboard.release(sendedKey.lower())
+							keyboard.release(Key.shift)
+						else:
+							pressAndRelease(sendedKey)
+					#Special key
+					elif tcpString[0:5] == '<SPK>':
+						specialKey = tcpString[5:]
+						specialKey = modifier.get(specialKey.lower(), 'err')
+						if specialKey != 'err':
+							pressAndRelease(specialKey)
+						else:
+							print('wrong key')
+					#combination of two keys
+					elif tcpString[0:8] == '<KCOMBO>':
+						#find first command
+						command1 = tcpString[8:tcpString.index('<AND>')]
+						if len(command1)>1:
+							command1 = modifier.get(command1.lower(), 'err')
+						#find second
+						command2 = tcpString[tcpString.index('<AND>')+5:].rstrip()
+						if len(command2)>1:
+							command2 = modifier.get(command2.lower(), 'err')
 
-	try:
-		# Receive the data and retransmit it
-		print('connection from', client_address)
-		while _LOOP:
-			data = connection.recv(160)
-			if data:
-				tcpString = data.decode()
-				print('Receiving: ', tcpString)
-				# Single key command
-				if tcpString[0:4] == '<SK>':
-					#somehow on mac capital letters returns: a
-					sendedKey = tcpString[4]
-					if sendedKey.isupper():
-						keyboard.press(Key.shift)
-						keyboard.press(sendedKey.lower())
-						keyboard.release(sendedKey.lower())
-						keyboard.release(Key.shift)
-					else:
-						pressAndRelease(sendedKey)
-				#Special key
-				elif tcpString[0:5] == '<SPK>':
-					specialKey = tcpString[5:]
-					specialKey = modifier.get(specialKey.lower(), 'err')
-					if specialKey != 'err':
-						pressAndRelease(specialKey)
-					else:
-						print('wrong key')
-				#combination of two keys
-				elif tcpString[0:8] == '<KCOMBO>':
-					#find first command
-					command1 = tcpString[8:tcpString.index('<AND>')]
-					if len(command1)>1:
-						command1 = modifier.get(command1.lower(), 'err')
-					#find second
-					command2 = tcpString[tcpString.index('<AND>')+5:].rstrip()
-					if len(command2)>1:
-						command2 = modifier.get(command2.lower(), 'err')
+						#if no error send the keycombo
+						if command1 != 'err' and command2 != 'err':
+							keyboard.press(command1)
+							keyboard.press(command2)
+							keyboard.release(command2)
+							keyboard.release(command1)
+						else:
+							print('wrong key')
 
-					#if no error send the keycombo
-					if command1 != 'err' and command2 != 'err':
-						keyboard.press(command1)
-						keyboard.press(command2)
-						keyboard.release(command2)
-						keyboard.release(command1)
-					else:
-						print('wrong key')
+					#only key down
+					elif tcpString[0:8] == '<KPRESS>':
+						pressed = tcpString[8:]
+						if len(pressed)>1:
+							pressed = modifier.get(pressed.lower(), 'err')
+						if pressed != 'err':
+							keyboard.press(pressed)
+						else:
+							print('wrong key')
 
-				#combination of three keys
-				elif tcpString[0:7] == '<KTRIO>':
-					#find first command
-					command1 = tcpString[7:tcpString.index('<AND>')]
-					if len(command1)>1:
-						command1 = modifier.get(command1.lower(), 'err')
-					command2 = tcpString[tcpString.index('<AND>')+5:tcpString.index('<AND2>')]
-					if len(command2)>1:
-						command2 = modifier.get(command2.lower(), 'err')
-					#find third
-					command3 = tcpString[tcpString.index('<AND2>')+6:].rstrip()
-					if len(command3)>1:
-						command3 = modifier.get(command3.lower(), 'err')
+					#only key up
+					elif tcpString[0:10] == '<KRELEASE>':
+						released = tcpString[10:]
+						if len(released)>1:
+							released = modifier.get(released.lower(), 'err')
+						if released != 'err':
+							keyboard.release(released)
+						else:
+							print('wrong key')
 
-					#if no error send the keycombo
-					if command1 != 'err' and command2 != 'err' and command3 != 'err':
-						keyboard.press(command1)
-						keyboard.press(command2)
-						keyboard.press(command3)
-						keyboard.release(command3)
-						keyboard.release(command2)
-						keyboard.release(command1)
-					else:
-						print('wrong key')
+					#send message
+					elif tcpString[0:5] == '<MSG>':
+						try:
+							keyboard.type(tcpString[5:])
+						except:
+							print("NOT ALLOWED")
 
-				#only key down
-				elif tcpString[0:8] == '<KPRESS>':
-					pressed = tcpString[8:]
-					if len(pressed)>1:
-						pressed = modifier.get(pressed.lower(), 'err')
-					if pressed != 'err':
-						keyboard.press(pressed)
-					else:
-						print('wrong key')
+					#open file
+					elif tcpString[0:6] == '<FILE>':
+						try:
+							openFile(tcpString[6:])
+						except:
+							print("error")
 
-				#only key up
-				elif tcpString[0:10] == '<KRELEASE>':
-					released = tcpString[10:]
-					if len(released)>1:
-						released = modifier.get(released.lower(), 'err')
-					if released != 'err':
-						keyboard.release(released)
-					else:
-						print('wrong key')
+					#only for testing/debug
+					elif tcpString[0:6] == '<STOP>':
+						msg = 'You have closed the application'
+						connection.sendall(msg.encode())
+						_LOOP = False
 
-				#send message
-				elif tcpString[0:5] == '<MSG>':
-					try:
-						keyboard.type(tcpString[5:])
-					except:
-						print("NOT ALLOWED")
-
-				#open file
-				elif tcpString[0:6] == '<FILE>':
-					try:
-						openFile(tcpString[6:])
-					except:
-						print("error")
-
-				#only for testing/debug
-				elif tcpString[0:6] == '<STOP>':
-					msg = 'You have closed the application'
+					msg = 'ok'
 					connection.sendall(msg.encode())
-					_LOOP = False
 
-				msg = 'ok'
-				connection.sendall(msg.encode())
+				else:
+					print('no more data from', client_address)
+					break
 
-			else:
-				print('no more data from', client_address)
-				break
+		finally:
+			# Clean up the connection
+			print('finalize this transmition')
+			connection.close()
 
-	finally:
-		# Clean up the connection
-		print('finalize this transmition')
-		connection.close()
+	print('shutdown program')
 
-print('shutdown program')
+if __name__ == "__main__":
+	#Put the listener in a Thread so we can do other stuff
+	t2 = Thread(target = myListener)
+	t2.setDaemon(True)
+	t2.start()
+	#Start the GUI part
+	myRumps()
+	while True:
+		pass
