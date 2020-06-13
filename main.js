2cbbf9dc15c5aaceb8392373128b718d278d8349
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2020-06-13 23:54:50 +0200

    tidy up

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ app.on('activate', () => {
 		createWindow()
 	}
 })
-if (process.platform == "darwin") {app.dock.setIcon(path.join(__dirname, 'img/logo.png'))};
+if (process.platform == "darwin") { app.dock.setIcon(path.join(__dirname, 'img/logo.png')) };
 
 function createListener() {
 	// Load socket
@@ -165,7 +165,7 @@ var portInUse = function (port, callback) {
 
 function findKeyCode(key) {
 	for (item of keys) {
-		if(key.toLowerCase() == item.key || key == item.vKeyCode) {
+		if (key.toLowerCase() == item.key || key == item.vKeyCode) {
 			return item.code
 			break;
 		}
@@ -176,23 +176,23 @@ function processKeyData(key, modifiers) {
 	let script = null;
 	let modifiersInString = '{'
 	for (item of modifiers) {
-		if(item == 'cmd') item = 'command';
-		modifiersInString += item+' down,';
+		if (item == 'cmd') item = 'command';
+		modifiersInString += item + ' down,';
 	}
-	modifiersInString = modifiersInString.substring(0, modifiersInString.length -1 )
+	modifiersInString = modifiersInString.substring(0, modifiersInString.length - 1)
 	modifiersInString += '}'
 
 	if (modifiers.length) {
-		if(key.length > 1) {
-			script = `key code ${findKeyCode(key)} using ${modifiersInString}` ;
+		if (key.length > 1) {
+			script = `key code ${findKeyCode(key)} using ${modifiersInString}`;
 		} else {
-			script = `keystroke \"${key}\" using ${modifiersInString}` ;
+			script = `keystroke \"${key}\" using ${modifiersInString}`;
 		}
 	} else {
-		if(key.length > 1) {
-			script = `key code ${findKeyCode(key)}` ;
+		if (key.length > 1) {
+			script = `key code ${findKeyCode(key)}`;
 		} else {
-			script = `keystroke \"${key}\"` ;
+			script = `keystroke \"${key}\"`;
 		}
 	}
 	console.log(script)
@@ -204,7 +204,7 @@ function processIncomingData(data) {
 	switch (data.type) {
 		case 'press':
 			if (process.platform == "darwin") {
-				osascript.execute('tell application \"System Events\"\n'+processKeyData(data.key, data.modifiers)+'\nend tell', function(err, result, raw){
+				osascript.execute('tell application \"System Events\"\n' + processKeyData(data.key, data.modifiers) + '\nend tell', function (err, result, raw) {
 					if (err) return console.error(err)
 					console.log(result, raw)
 				});
@@ -223,22 +223,22 @@ function processIncomingData(data) {
 
 		case 'processOSX':
 			let script = null;
-			if(data.processName == 'null' || data.processName == '') {
+			if (data.processName == 'null' || data.processName == '') {
 				if (process.platform == "darwin") {
-					osascript.execute('tell application \"System Events\"\n'+processKeyData(data.key, data.modifiers)+'\nend tell', function(err, result, raw){
+					osascript.execute('tell application \"System Events\"\n' + processKeyData(data.key, data.modifiers) + '\nend tell', function (err, result, raw) {
 						if (err) return console.error(err)
 						console.log(result, raw)
 					});
-				} 
+				}
 			} else {
 				if (process.platform == "darwin") {
-					osascript.execute(`tell application \"System Events\"\ntell process \"${data.processName}\"\nset frontmost to true\n`+processKeyData(data.key, data.modifiers)+'\nend tell\nend tell', function(err, result, raw){
+					osascript.execute(`tell application \"System Events\"\ntell process \"${data.processName}\"\nset frontmost to true\n` + processKeyData(data.key, data.modifiers) + '\nend tell\nend tell', function (err, result, raw) {
 						if (err) return console.error(err)
 						console.log(result, raw)
 					});
-				} 
+				}
 			}
-			
+
 			break;
 
 		case 'shell':
@@ -262,7 +262,7 @@ function processIncomingData(data) {
 		case 'file':
 			if (process.platform == "darwin") {
 
-				child_process.exec('open '+data.path, function (err, stdout, stderr) {
+				child_process.exec('open ' + data.path, function (err, stdout, stderr) {
 					if (err) {
 						console.error(err);
 						return;
@@ -271,7 +271,7 @@ function processIncomingData(data) {
 					process.exit(0);// exit process once it is opened
 				})
 			} else {
-				child_process.exec('start '+data.path, function (err, stdout, stderr) {
+				child_process.exec('start ' + data.path, function (err, stdout, stderr) {
 					if (err) {
 						console.error(err);
 						return;
@@ -344,111 +344,111 @@ function hitHotkey(key, modifier) {
 }
 
 const keys = [
-	{key: 'cmd', code: 55, vKeyCode: '0x37'},
-	{key: 'command', code: 55, vKeyCode: '0x37'},
-	{key: 'alt', code: 58, vKeyCode: '0x3A'},
-	{key: 'option', code: 58, vKeyCode: '0x3A'},
-	{key: 'ctrl', code: 59, vKeyCode: '0x3B'},
-	{key: 'control', code: 59, vKeyCode: '0x3B'},
-	{key: 'shift', code: 56, vKeyCode: '0x38'},
-	{key: 'right_shift', code: 60, vKeyCode: '0x3C'},
-	{key: 'right_alt', code: 61, vKeyCode: '0x3D'},
-	{key: 'right_ctrl', code: 62, vKeyCode: '0x3E'},
-	{key: 'fn', code: 63, vKeyCode: '0X3F'},
+	{ key: 'cmd', code: 55, vKeyCode: '0x37' },
+	{ key: 'command', code: 55, vKeyCode: '0x37' },
+	{ key: 'alt', code: 58, vKeyCode: '0x3A' },
+	{ key: 'option', code: 58, vKeyCode: '0x3A' },
+	{ key: 'ctrl', code: 59, vKeyCode: '0x3B' },
+	{ key: 'control', code: 59, vKeyCode: '0x3B' },
+	{ key: 'shift', code: 56, vKeyCode: '0x38' },
+	{ key: 'right_shift', code: 60, vKeyCode: '0x3C' },
+	{ key: 'right_alt', code: 61, vKeyCode: '0x3D' },
+	{ key: 'right_ctrl', code: 62, vKeyCode: '0x3E' },
+	{ key: 'fn', code: 63, vKeyCode: '0X3F' },
 
-	{key: 'f1', code: 122, vKeyCode: '0x7A'},
-	{key: 'f2', code: 120, vKeyCode: '0x78'},
-	{key: 'f3', code: 99, vKeyCode: '0x63'},
-	{key: 'f4', code: 118, vKeyCode: '0x76'},
-	{key: 'f5', code: 96, vKeyCode: '0x60'},
-	{key: 'f6', code: 97, vKeyCode: '0x61'},
-	{key: 'f7', code: 98, vKeyCode: '0x62'},
-	{key: 'f8', code: 99, vKeyCode: '0x64'},
-	{key: 'f9', code: 100, vKeyCode: '0x65'},
-	{key: 'f10', code: 109, vKeyCode: '0x6D'},
-	{key: 'f11', code: 103, vKeyCode: '0x67'},
-	{key: 'f12', code: 111, vKeyCode: '0x6F'},
-	{key: 'f13', code: 105, vKeyCode: '0x69' },
-	{key: 'f14', code: 107, vKeyCode: '0x6B' },
-	{key: 'f15', code: 113, vKeyCode: '0x71' },
-	{key: 'f16', code: 106, vKeyCode: '0x6A' },
-	{key: 'f17', code: 64, vKeyCode: '0x40' },
-	{key: 'f18', code: 79, vKeyCode: '0x4F' },
-	{key: 'f19', code: 80, vKeyCode: '0x50' },
-	{key: 'f20', code: 90, vKeyCode: '0x5A' },
+	{ key: 'f1', code: 122, vKeyCode: '0x7A' },
+	{ key: 'f2', code: 120, vKeyCode: '0x78' },
+	{ key: 'f3', code: 99, vKeyCode: '0x63' },
+	{ key: 'f4', code: 118, vKeyCode: '0x76' },
+	{ key: 'f5', code: 96, vKeyCode: '0x60' },
+	{ key: 'f6', code: 97, vKeyCode: '0x61' },
+	{ key: 'f7', code: 98, vKeyCode: '0x62' },
+	{ key: 'f8', code: 99, vKeyCode: '0x64' },
+	{ key: 'f9', code: 100, vKeyCode: '0x65' },
+	{ key: 'f10', code: 109, vKeyCode: '0x6D' },
+	{ key: 'f11', code: 103, vKeyCode: '0x67' },
+	{ key: 'f12', code: 111, vKeyCode: '0x6F' },
+	{ key: 'f13', code: 105, vKeyCode: '0x69' },
+	{ key: 'f14', code: 107, vKeyCode: '0x6B' },
+	{ key: 'f15', code: 113, vKeyCode: '0x71' },
+	{ key: 'f16', code: 106, vKeyCode: '0x6A' },
+	{ key: 'f17', code: 64, vKeyCode: '0x40' },
+	{ key: 'f18', code: 79, vKeyCode: '0x4F' },
+	{ key: 'f19', code: 80, vKeyCode: '0x50' },
+	{ key: 'f20', code: 90, vKeyCode: '0x5A' },
 
-	{key: 'escape', code: 53, vKeyCode: '0x35'},
-	{key: 'esc', code: 53, vKeyCode: '0x35'},
-	{key: 'enter', code: 36, vKeyCode: '0x24'},
-	{key: 'return', code: 76, vKeyCode: '0x24'},
-	{key: 'space', code: 49, vKeyCode: '0x31'},
-	{key: 'spacebar', code: 49, vKeyCode: '0x31'},
+	{ key: 'escape', code: 53, vKeyCode: '0x35' },
+	{ key: 'esc', code: 53, vKeyCode: '0x35' },
+	{ key: 'enter', code: 36, vKeyCode: '0x24' },
+	{ key: 'return', code: 76, vKeyCode: '0x24' },
+	{ key: 'space', code: 49, vKeyCode: '0x31' },
+	{ key: 'spacebar', code: 49, vKeyCode: '0x31' },
 
-	{key: 'delete', code: 51, vKeyCode: '0x33'},
-	{key: 'backspace', code: 117},
-	{key: 'up', code: 126, vKeyCode: '0x7E'},
-	{key: 'down', code: 125, vKeyCode: '0x7D'},
-	{key: 'left', code: 123, vKeyCode: '0x7B'},
-	{key: 'right', code: 124, vKeyCode: '0x7C'},
-	{key: 'home', code: 115, vKeyCode: '0x73'},
-	{key: 'end', code: 119, vKeyCode: '0x77'},
-	{key: 'page_up', code: 116, vKeyCode: '0x74'},
-	{key: 'page_down', code: 121, vKeyCode: '0x79'},
+	{ key: 'delete', code: 51, vKeyCode: '0x33' },
+	{ key: 'backspace', code: 117 },
+	{ key: 'up', code: 126, vKeyCode: '0x7E' },
+	{ key: 'down', code: 125, vKeyCode: '0x7D' },
+	{ key: 'left', code: 123, vKeyCode: '0x7B' },
+	{ key: 'right', code: 124, vKeyCode: '0x7C' },
+	{ key: 'home', code: 115, vKeyCode: '0x73' },
+	{ key: 'end', code: 119, vKeyCode: '0x77' },
+	{ key: 'page_up', code: 116, vKeyCode: '0x74' },
+	{ key: 'page_down', code: 121, vKeyCode: '0x79' },
 
-	{key: 'Keypad0', code: 82, vKeyCode: '0x52' },
-	{key: 'Keypad1', code: 83, vKeyCode: '0x53' },
-	{key: 'Keypad2', code: 84, vKeyCode: '0x54' },
-	{key: 'Keypad3', code: 85, vKeyCode: '0x55' },
-	{key: 'Keypad4', code: 86, vKeyCode: '0x56' },
-	{key: 'Keypad5', code: 87, vKeyCode: '0x57' },
-	{key: 'Keypad6', code: 88, vKeyCode: '0x58' },
-	{key: 'Keypad7', code: 89, vKeyCode: '0x59' },
-	{key: 'Keypad8', code: 91, vKeyCode: '0x5B' },
-	{key: 'Keypad9', code: 92, vKeyCode: '0x5C' },
+	{ key: 'Keypad0', code: 82, vKeyCode: '0x52' },
+	{ key: 'Keypad1', code: 83, vKeyCode: '0x53' },
+	{ key: 'Keypad2', code: 84, vKeyCode: '0x54' },
+	{ key: 'Keypad3', code: 85, vKeyCode: '0x55' },
+	{ key: 'Keypad4', code: 86, vKeyCode: '0x56' },
+	{ key: 'Keypad5', code: 87, vKeyCode: '0x57' },
+	{ key: 'Keypad6', code: 88, vKeyCode: '0x58' },
+	{ key: 'Keypad7', code: 89, vKeyCode: '0x59' },
+	{ key: 'Keypad8', code: 91, vKeyCode: '0x5B' },
+	{ key: 'Keypad9', code: 92, vKeyCode: '0x5C' },
 
-	{key: 'KeypadDecimal', code: 65, vKeyCode: '0x41' },
-	{key: 'KeypadMultiply', code: 67, vKeyCode: '0x43' },
-	{key: 'KeypadPlus', code: 69, vKeyCode: '0x45' },
-	{key: 'KeypadClear', code: 71, vKeyCode: '0x47' },
-	{key: 'KeypadDivide', code: 75, vKeyCode: '0x4B' },
-	{key: 'KeypadEnter', code: 76, vKeyCode: '0x4C' },
-	{key: 'KeypadMinus', code: 78, vKeyCode: '0x4E' },
-	{key: 'KeypadEquals', code: 81, vKeyCode: '0x51' },
+	{ key: 'KeypadDecimal', code: 65, vKeyCode: '0x41' },
+	{ key: 'KeypadMultiply', code: 67, vKeyCode: '0x43' },
+	{ key: 'KeypadPlus', code: 69, vKeyCode: '0x45' },
+	{ key: 'KeypadClear', code: 71, vKeyCode: '0x47' },
+	{ key: 'KeypadDivide', code: 75, vKeyCode: '0x4B' },
+	{ key: 'KeypadEnter', code: 76, vKeyCode: '0x4C' },
+	{ key: 'KeypadMinus', code: 78, vKeyCode: '0x4E' },
+	{ key: 'KeypadEquals', code: 81, vKeyCode: '0x51' },
 
-	{key: 'A', code: 0, vKeyCode: '0x00' },
-	{key: 'B', code: 11, vKeyCode: '0x0B' },
-	{key: 'C', code: 8, vKeyCode: '0x08' },
-	{key: 'D', code: 2, vKeyCode: '0x02' },
-	{key: 'E', code: 14, vKeyCode: '0x0E' },
-	{key: 'F', code: 3, vKeyCode: '0x03' },
-	{key: 'G', code: 5, vKeyCode: '0x05' },
-	{key: 'H', code: 4, vKeyCode: '0x04' },
-	{key: 'I', code: 34, vKeyCode: '0x22' },
-	{key: 'J', code: 38, vKeyCode: '0x26' },
-	{key: 'K', code: 40, vKeyCode: '0x28' },
-	{key: 'L', code: 37, vKeyCode: '0x25' },
-	{key: 'M', code: 46, vKeyCode: '0x2E' },
-	{key: 'N', code: 45, vKeyCode: '0x2D' },
-	{key: 'O', code: 31, vKeyCode: '0x1F' },
-	{key: 'P', code: 35, vKeyCode: '0x23' },
-	{key: 'Q', code: 12, vKeyCode: '0x0C' },
-	{key: 'R', code: 15, vKeyCode: '0x0F' },
-	{key: 'S', code: 1, vKeyCode: '0x01' },
-	{key: 'T', code: 17, vKeyCode: '0x11' },
-	{key: 'U', code: 32, vKeyCode: '0x20' },
-	{key: 'V', code: 9, vKeyCode: '0x09' },
-	{key: 'W', code: 13, vKeyCode: '0x0D' },
-	{key: 'X', code: 7, vKeyCode: '0x07' },
-	{key: 'Y', code: 16, vKeyCode: '0x10' },
-	{key: 'Z', code: 6, vKeyCode: '0x06' },
-	{key: '0', code: 29, vKeyCode: '0x1D' },
-	{key: '1', code: 18, vKeyCode: '0x12' },
-	{key: '2', code: 19, vKeyCode: '0x13' },
-	{key: '3', code: 20, vKeyCode: '0x14' },
-	{key: '4', code: 21, vKeyCode: '0x15' },
-	{key: '5', code: 23, vKeyCode: '0x17' },
-	{key: '6', code: 22, vKeyCode: '0x16' },
-	{key: '7', code: 26, vKeyCode: '0x1A' },
-	{key: '8', code: 28, vKeyCode: '0x1C' },
-	{key: '9', code: 25, vKeyCode: '0x19' },
+	{ key: 'A', code: 0, vKeyCode: '0x00' },
+	{ key: 'B', code: 11, vKeyCode: '0x0B' },
+	{ key: 'C', code: 8, vKeyCode: '0x08' },
+	{ key: 'D', code: 2, vKeyCode: '0x02' },
+	{ key: 'E', code: 14, vKeyCode: '0x0E' },
+	{ key: 'F', code: 3, vKeyCode: '0x03' },
+	{ key: 'G', code: 5, vKeyCode: '0x05' },
+	{ key: 'H', code: 4, vKeyCode: '0x04' },
+	{ key: 'I', code: 34, vKeyCode: '0x22' },
+	{ key: 'J', code: 38, vKeyCode: '0x26' },
+	{ key: 'K', code: 40, vKeyCode: '0x28' },
+	{ key: 'L', code: 37, vKeyCode: '0x25' },
+	{ key: 'M', code: 46, vKeyCode: '0x2E' },
+	{ key: 'N', code: 45, vKeyCode: '0x2D' },
+	{ key: 'O', code: 31, vKeyCode: '0x1F' },
+	{ key: 'P', code: 35, vKeyCode: '0x23' },
+	{ key: 'Q', code: 12, vKeyCode: '0x0C' },
+	{ key: 'R', code: 15, vKeyCode: '0x0F' },
+	{ key: 'S', code: 1, vKeyCode: '0x01' },
+	{ key: 'T', code: 17, vKeyCode: '0x11' },
+	{ key: 'U', code: 32, vKeyCode: '0x20' },
+	{ key: 'V', code: 9, vKeyCode: '0x09' },
+	{ key: 'W', code: 13, vKeyCode: '0x0D' },
+	{ key: 'X', code: 7, vKeyCode: '0x07' },
+	{ key: 'Y', code: 16, vKeyCode: '0x10' },
+	{ key: 'Z', code: 6, vKeyCode: '0x06' },
+	{ key: '0', code: 29, vKeyCode: '0x1D' },
+	{ key: '1', code: 18, vKeyCode: '0x12' },
+	{ key: '2', code: 19, vKeyCode: '0x13' },
+	{ key: '3', code: 20, vKeyCode: '0x14' },
+	{ key: '4', code: 21, vKeyCode: '0x15' },
+	{ key: '5', code: 23, vKeyCode: '0x17' },
+	{ key: '6', code: 22, vKeyCode: '0x16' },
+	{ key: '7', code: 26, vKeyCode: '0x1A' },
+	{ key: '8', code: 28, vKeyCode: '0x1C' },
+	{ key: '9', code: 25, vKeyCode: '0x19' },
 ]
